a1393e5f78f17453d2bb7936821bfb5a4d7357b9
ruku786 <57506508+ruku786@users.noreply.github.com>
2023-08-04 12:06:56 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,29 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, count, sum
+
+# Create a Spark session
+spark = SparkSession.builder.appName("EcommerceProcessing").getOrCreate()
+
+# Load the CSV files into DataFrames
+retail_df = spark.read.csv("all_csv_files.csv", header=True, inferSchema=True)
+
+
+# Perform joins to associate data
+order_details_df = orders_df.join(customers_df, "customer_id", "inner") \
+    .withColumnRenamed("name", "customer_name") \
+    .join(products_df, orders_df.products.keys()[0], "inner") \
+    .withColumnRenamed("name", "product_name")
+
+# Grouping, Aggregation, and Summarization
+order_summary_df = order_details_df.groupBy("customer_name") \
+    .agg(count("order_id").alias("order_count"),
+         sum("total_amount").alias("total_spent"))
+
+# Filtering
+out_of_stock_products_df = products_df.filter(col("stock_quantity") <= 0)
+
+# 2.Save the transformed data to a Parquet file
+transformed_data_df.write.parquet("transformed_data.parquet")
+
+# Stop the Spark session
+spark.stop()
